7b2640643d89f29e05e67a630c41eb645963bc02
Sergio Gabriel Benitez <ben.gabriel.w@gmail.com>
2021-05-13 20:34:49 -0300

    add workign button

--- CSS-General-Practice/corners-js.js
+++ CSS-General-Practice/corners-js.js
@@ -1,16 +1,19 @@
 
 document.getElementById('containerBox').style.position = 'absolute';
 
-document.getElementById('positionChangerBtn').onclick = changePositioning(document.getElementById('containerBox'));
+document.getElementById('positionChangerBtn').onclick = changePositioning;
 
-function changePositioning(classToModify) {
+function changePositioning() {
     
-    let containerPosition = classToModify.style.position;
+    classToModify = document.getElementById('containerBox');
 
-    if(containerPosition == 'absolute'){
+    let containerPosition = classToModify.style.position;
+    
+    
+    if(containerPosition === 'absolute'){
         classToModify.style.position = 'static'
     }
-    else if(containerPosition == 'static'){
+    else if(containerPosition === 'static'){
         classToModify.style.position = 'absolute'
     }
     
