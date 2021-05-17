8531a6a1f98cf79b2acfdf768e26d6e21903a300
Sergio Gabriel Benitez <ben.gabriel.w@gmail.com>
2021-05-17 08:10:28 -0300

    Add start button event listener

--- MS-Typing/script.js
+++ MS-Typing/script.js
@@ -1,5 +1,7 @@
 
 // This project is based on the Chapter 4 of the Microsoft WebDev self learning course, from GitHub.
+// It's a bit over-commented because I wanted to research/comprehend every term I came across
+// I didn't want to just copy/paste the solution given.
 
 // Array of quotes that are gonna be used:
 const quotes = [
@@ -13,7 +15,7 @@ const quotes = [
 
 let words = []; // Array to store the current quote being typed
 
-let wordIndex = 0; // Variable to keep count of the current word's index (in the words[] array)  
+let wordIndex; // Variable to keep count of the current word index (in the words[] array)  
 
 let startTime = Date.now(); 
 
@@ -27,11 +29,25 @@ const typedValueElement = document.getElementById('typed-value');
 document.getElementById('start').addEventListener('click', () =>{
 
     const quoteIndex = Math.floor(Math.random() * quotes.length);
-    // Math.random gives a random float between 0 and 1
+    // Math.random returns a random float between 0 and 1
     // quotes.length acts as the maximun value posible (1 * ql = ql)
     // Math.floor rounds the decimals downwards assigning quoteIndex a valid value
 
+    const quote = quotes[quoteIndex]; // Holds current quote as a string
+
+    words = quote.split(' '); // Separetes and saves each word in the words[] array
+    wordIndex = 0;
+
+    const spanWords = words.map(function (word){ return `<span>${word} </span>` });
+    // array.map() returns a new array, with elements based on the returned value 
+    // of a function executed over each element of the original array.
+
+    quoteElement.innerHTML = spanWords.join(' ');
+    // Replace the content in the DOM with the resulting string from array.join()
     
+    quoteElement.childNodes[0].className = 'highlight';    
+
+
 
 })
 
