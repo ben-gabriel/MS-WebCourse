62a8b6c786abe1ae79de4e6ceff6f20ada0cef72
Sergio Gabriel Benitez <ben.gabriel.w@gmail.com>
2021-05-21 02:12:00 -0300

    delete comments

--- Event-Practice/script.js
+++ Event-Practice/script.js
@@ -1,11 +1,8 @@
 // The purpose of this script is to practice the use of event driven
 //  actions, the main idea is to have a button which when clicked,
 //  triggers a change in color on the child number 2.
-// In order to practice asynchronicity too, while the event driven
-//  action happens, the child number 1 should be simultaneously
-//  changing color on its own, every x amount of time (e.g. 1 sec).
 // An interesting way to expand this idea in the future, would be 
-//  to make te change between colors a fading transtition, or similar.
+//  to make the change between colors a fading transtition, or similar.
 
 
 const child1 = document.getElementById('c1');
@@ -40,23 +37,3 @@ btn.addEventListener('mouseup', () => {
 setInterval(() => {
     changeColorRandom(child1);
 }, 1000);
-
-
-
-
-/*
-function loop(){
-    
-    let int1, int2, int3;
-    for (let i = 0; i < 10; i++) {
-        int1 = getRandomInt(255);    
-        int2 = getRandomInt(255);    
-        int3 = getRandomInt(255);    
-       
-        console.log(i);
-        changeColor(child1,int1,int2,int3);
-        //console.log(int1);
-    }
-
-}*/
-
